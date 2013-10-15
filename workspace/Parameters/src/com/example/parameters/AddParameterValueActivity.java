49a9747a70a2399b94b662ce1a23a22836c7fd3e
PrabhanshuShekhar <prabhanshu@prabhanshu-Inspiron-620s.(none)>
2013-10-15 19:37:51 +0530

    commited on 15-oct

--- workspace/Parameters/src/com/example/parameters/AddParameterValueActivity.java
+++ workspace/Parameters/src/com/example/parameters/AddParameterValueActivity.java
@@ -1,9 +1,14 @@
 package com.example.parameters;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -22,10 +27,11 @@ public class AddParameterValueActivity extends Activity {
 	TextView slider,slider_value;
 	SeekBar seekbar;
 	String table_name;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Parse.initialize(this, "hjYMRHgjBNK6fzcltOMtnmglaDYIQIU3PJfdCMF3", "xgBSMsHThQK5kzLvqSwDznSrpH9Gq8bW7ZYl6YoA");
+		
 		setContentView(R.layout.activity_add_parameter_value);
 		slider_value = (TextView) findViewById(R.id.slider_value);
 		 slider = (TextView) findViewById(R.id.slider);
@@ -33,8 +39,10 @@ public class AddParameterValueActivity extends Activity {
 		table_name = getIntent().getStringExtra("location");
 		 seekbar = (SeekBar) findViewById(R.id.seekbar);
 		seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+			
 			public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser)
 			{
+			
 				slider_value.setText(""+progress);
 			}
 			
@@ -54,33 +62,51 @@ public class AddParameterValueActivity extends Activity {
 	{
 		// insert parameter values in db
 		String location_objectid;
+		Date date = new Date();
+    	SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 		Parse.initialize(AddParameterValueActivity.this, "hjYMRHgjBNK6fzcltOMtnmglaDYIQIU3PJfdCMF3", "xgBSMsHThQK5kzLvqSwDznSrpH9Gq8bW7ZYl6YoA");
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("ParameterValue");
 		query.whereEqualTo("location_objectid", table_name);
+		query.whereEqualTo("create_date", df.format(date));
 		query.findInBackground(new FindCallback<ParseObject>() {
 			  public void done(List<ParseObject> locationList, ParseException e) {
 			    // locationList has record 
 				  if (e == null) {   // update based on object id in parameter values table
-//					  ParseObject location = new ParseObject("ParameterValue");
-//						location.put("location_objectid", locationList.get(0).getObjectId().toString());
-//						location.put(slider.getText().toString(),Float.parseFloat(slider_value.getText().toString()));
-//						location.put("Create_date", "12/10/2013");
-//						location.saveInBackground();
-//						finish();
-					  
+//					  
+					  if(locationList.size()>0){
 					  ParseQuery<ParseObject> update_query = ParseQuery.getQuery("ParameterValue");
 					  update_query.getInBackground(locationList.get(0).getObjectId(),new GetCallback<ParseObject>() {
 						  public void done(ParseObject gameScore, ParseException e) {
 							    if (e == null) {
 							      // Now let's update it with some new data. In this case, only cheatMode and score
 							      // will get sent to the Parse Cloud. playerName hasn't changed.
+							    	
 							      gameScore.put(slider.getText().toString(),Float.parseFloat(slider_value.getText().toString()));
-							      gameScore.put("create_date", "12/10/2013");
+							      //gameScore.put("create_date", df.format(date));
 							      gameScore.saveInBackground();
+							      Intent intent1 = new Intent(AddParameterValueActivity.this,AddRecordActivity.class);
+							      intent1.putExtra("location", table_name);
+//							      intent1.putExtra("param_name",slider.getText().toString() );
+//							      intent1.putExtra("param_value",slider_value.getText().toString());
+							      startActivity(intent1);
 							      finish();
-							    }
+							    	}
 							  }
 							});
+					  }
+					  else
+					  {
+						  Date date = new Date();
+					    	SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+						    ParseObject location = new ParseObject("ParameterValue");
+							location.put("location_objectid", table_name);
+							location.put(slider.getText().toString(),Float.parseFloat(slider_value.getText().toString()));
+							location.put("create_date", df.format(date));
+							location.saveInBackground();
+							Intent intent1 = new Intent(AddParameterValueActivity.this,AddRecordActivity.class);
+						    startActivity(intent1);
+							finish();  
+					  }
 					  
 			        } else {
 			            Log.d("record", "Error: " + e.getMessage());
--- workspace/Parameters/src/com/example/parameters/AddRecordActivity.java
+++ workspace/Parameters/src/com/example/parameters/AddRecordActivity.java
@@ -1,33 +1,123 @@
 package com.example.parameters;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.security.auth.callback.Callback;
+
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddRecordActivity extends Activity  {
   Intent intent;
-  String param_name;
+  String param_name,location_name,param_value;
+  static final int GET_PARAMETER_VALUE = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.record_add);
 		RelativeLayout outer_RL = (RelativeLayout) findViewById(R.id.parameter);
-		param_name = getIntent().getStringExtra("location");
-//		outer_RL.setOnClickListener(this);
-//		setContentView(R.layout.activity_add_record);
-//		GridView gridview1 = (GridView)findViewById(R.id.gridview1);
-//		gridview1.setAdapter(new ParameterAdapters(this));
+    	location_name = getIntent().getStringExtra("location");
+    	Date date = new Date();
+    	SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    	try{
+    	Parse.initialize(AddRecordActivity.this, "hjYMRHgjBNK6fzcltOMtnmglaDYIQIU3PJfdCMF3", "xgBSMsHThQK5kzLvqSwDznSrpH9Gq8bW7ZYl6YoA");
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("ParameterValue");
+        query.whereEqualTo("location_objectid", location_name);
+        query.whereEqualTo("create_date",df.format(date));
+      query.findInBackground(new FindCallback<ParseObject>() {
+			  public void done(List<ParseObject> objects, ParseException e) {
+      		if (e == null && objects.size()>0)
+      		{
+      			TextView ammonia = (TextView) findViewById(R.id.ammonia_value);
+      			if(objects.get(0).getNumber("NH4")!= null)
+      			{
+      			ammonia.setText(objects.get(0).getNumber("NH4").toString());
+      			}
+      			Log.d("record","===="+objects.get(0).getNumber("NH4"));
+      			TextView biochemical = (TextView) findViewById(R.id.biochemical_value);
+      			if(objects.get(0).getNumber("BOD")!= null)
+      			{
+      			biochemical.setText(objects.get(0).getNumber("BOD").toString());
+      			}
+      			
+      			
+      			TextView chloride = (TextView) findViewById(R.id.chloride_value);
+      			if(objects.get(0).getNumber("CL")!= null)
+      			{
+      			chloride.setText(objects.get(0).getNumber("CL").toString());
+      			}
+      			
+      			TextView colorimeter = (TextView)findViewById(R.id.colorimeter_value);
+      			if(objects.get(0).getNumber("CP")!= null)
+      			{
+      			colorimeter.setText(objects.get(0).getNumber("CP").toString());
+      			}
+      			
+      			TextView conductivity = (TextView) findViewById(R.id.conductivity_value);
+      			if(objects.get(0).getNumber("CTVT")!= null)
+      			{
+      			conductivity.setText(objects.get(0).getNumber("CTVT").toString());
+      			}
+      			
+      			TextView dissolved_oxygen = (TextView) findViewById(R.id.dissolved_oxygen_value);
+      			if(objects.get(0).getNumber("DO")!= null)
+      			{
+      			dissolved_oxygen.setText(objects.get(0).getNumber("DO").toString());
+      			}
+      			
+      			TextView  free_chlorine = (TextView)findViewById(R.id.free_chlorine_value);
+      			if(objects.get(0).getNumber("FCL")!= null)
+      			{
+      			free_chlorine.setText(objects.get(0).getNumber("FCL").toString());
+      			}
+      			
+      			TextView nitrate = (TextView) findViewById(R.id.nitrate_value);
+      			if(objects.get(0).getNumber("NO3")!= null)
+      			{
+      			nitrate.setText(objects.get(0).getNumber("NO3").toString());
+      			}
+      			
+      			TextView orp = (TextView) findViewById(R.id.orp_value);
+      			if(objects.get(0).getNumber("ORP")!= null)
+      			{
+      			orp.setText(objects.get(0).getNumber("ORP").toString());
+      			}
+      			
+      			TextView ph = (TextView) findViewById(R.id.ph_value);
+      			if(objects.get(0).getNumber("PH")!= null)
+      			{
+      			ph.setText(objects.get(0).getNumber("PH").toString());
+      			}
+      		}
+      	}
+		});
+    }
+    	catch(Exception e)
+    	{
+    		
+    	}
+
 	}
 	
-//	public void onClick(View arg0) {
-//	    Toast.makeText(this,"hi:", Toast.LENGTH_SHORT).show();
-//	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,65 +134,66 @@ public class AddRecordActivity extends Activity  {
 //			Toast.makeText(this,"id:"+"water_level", Toast.LENGTH_SHORT).show();
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "NH4");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 			break;
 			
 		case R.id.biochemical:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "BOD");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 			 	break;
 		case R.id.chloride:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "CL");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;	
 		case R.id.colorimeter:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "CP");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		case R.id.conductivity:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "CTVT");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		case R.id.dissolved_oxygen:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "DO");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		case R.id.free_chlorine:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "FCL");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		case R.id.nitrate:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "NO3");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		case R.id.orp:
 			intent = new Intent(this, AddParameterValueActivity.class);
 		    intent.putExtra("param_name", "ORP");
-		    intent.putExtra("location", param_name);
-		    startActivity(intent);Toast.makeText(this, "id:Nitrate", Toast.LENGTH_SHORT).show();
+		    intent.putExtra("location", location_name);
+		    startActivity(intent);
 		 	break;
 		case R.id.ph:
 			intent = new Intent(this, AddParameterValueActivity.class);
 			intent.putExtra("param_name", "PH");
-			intent.putExtra("location", param_name);
+			intent.putExtra("location", location_name);
 			startActivity(intent);
 		 	break;
 		}
 		
 	}
+  
 }
