30f4f7801e9e787a9c23ae62dd865c1088870658
pranavmahajan21 <pranav@motifworks.com>
2013-11-19 12:37:07 +0530

    email

--- workspace/Parameters/src/com/example/parameters/SettingsActivity.java
+++ workspace/Parameters/src/com/example/parameters/SettingsActivity.java
@@ -2,15 +2,18 @@ package com.example.parameters;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
-public class SettingsActivity extends Activity{
+import com.mw.aquatrack.email.Mail;
+
+public class SettingsActivity extends Activity {
 	TextView manageLocationTextView;
 	TextView manageParameterTextView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,23 +24,35 @@ public class SettingsActivity extends Activity{
 		manageParameterTextView = (TextView) findViewById(R.id.manage_parameter_textView);
 	}
 
-	public void onManageLocation(View view)
-	{
+	public void onManageLocation(View view) {
 		manageLocationTextView.setClickable(false);
 		startActivity(new Intent(SettingsActivity.this, ManageLocation.class));
 	}
-	
-	public void onManageParameter(View view)
-	{
+
+	public void onManageParameter(View view) {
 		manageParameterTextView.setClickable(false);
 		startActivity(new Intent(SettingsActivity.this, ManageParameter.class));
 	}
 
-	public void onEmail(View view)
-	{
-//		startActivity(new Intent(SettingsActivity.this, TempActivity.class));
+	public void onEmail(View view) {
+		AsyncExample asyncExample = new AsyncExample();
+		asyncExample.execute(new String[] { "hello wrold" });
+
 	}
-	
+
+	private class AsyncExample extends AsyncTask<String, Void, Void> {
+		@Override
+		protected Void doInBackground(String... params) {
+			Mail mail = new Mail();
+			try {
+				mail.send();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
 	@Override
 	protected void onResume() {
 		manageLocationTextView.setClickable(true);
